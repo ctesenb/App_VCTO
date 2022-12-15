f751dfa76df1c2989a46afdfce0c74c21f9c9605
Hyre <74812707+ctesenb@users.noreply.github.com>
2022-12-15 13:00:19 -0500

    Update internal_connection.dart

--- lib/src/connection/internal_connection.dart
+++ lib/src/connection/internal_connection.dart
@@ -1,11 +1,11 @@
 import 'package:mysql1/mysql1.dart';
 
 class RucConnection {
-  static String host = 'mysql-43314-0.cloudclusters.net',
-      user = 'admin',
-      password = 'adbO1NJG',
-      db = 'neo-deter';
-  static int port = 19751;
+  static String host = '',
+      user = '',
+      password = '',
+      db = '';
+  static int port = ;
   RucConnection();
   Future<MySqlConnection> getConnection() async {
     var settings = ConnectionSettings(
